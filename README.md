62f4e6afdfab7198c88bd14bf379e0c5f183d85f
golaughlin <gage.olaughlin@gmail.com>
2023-08-11 23:15:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# todo-list-react-ts
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-sudiv9)
---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  const [todos, setTodos] = React.useState(() => {
+    const savedTodos = localStorage.getItem('todos');
+    if (savedTodos) {
+      return JSON.parse(savedTodos);
+    } else {
+      return [];
+    }
+  });
+
+  const [todoText, setTodoText] = React.useState('');
+
+  React.useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo() {
+    if (todoText.trim() === '') return;
+
+    const newTodo = { id: Date.now(), text: todoText };
+    setTodos([...todos, newTodo]);
+    setTodoText('');
+  }
+
+  function removeTodo(todo: { id: number }) {
+    const filteredTodos = todos.filter((t) => t.id !== todo.id);
+    setTodos([...filteredTodos]);
+  }
+
+  const listItems = todos.map((todo) => (
+    <li key={todo.id}>
+      {todo.text}&nbsp;
+      <button onClick={() => removeTodo(todo)}>❌</button>
+    </li>
+  ));
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      {todos.length < 1 ? (
+        <div>
+          <h2>There are no todos on the list.</h2>
+          <p>Please add a new todo to the list.</p>
+        </div>
+      ) : (
+        <ul>{listItems}</ul>
+      )}
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addTodo();
+        }}
+      >
+        <input
+          type="text"
+          value={todoText}
+          onChange={(e) => setTodoText(e.target.value)}
+          placeholder="What do you need to do?"
+        />
+        <button type="submit">Add todo</button>
+      </form>
+    </div>
+  );
+}
